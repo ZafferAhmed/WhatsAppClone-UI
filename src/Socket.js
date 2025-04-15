dce40039f58e8345d2f1719537abfe89b20d5bee
Zaffer Ahmed <139559133+ZafferAhmed@users.noreply.github.com>
2025-04-15 16:54:49 +0530

    Bug fixed

--- src/Socket.js
+++ src/Socket.js
@@ -1,8 +1,11 @@
 import { io } from "socket.io-client";
-export const socket = io("http://18.144.169.213:4000", {
+
+const socket = io("http://18.144.169.213:4000", {
   transports: ["websocket"],
   withCredentials: true,
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
 });
+
+export default socket;
